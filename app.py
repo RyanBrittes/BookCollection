8bec1e4a966e60b0fa1fea14bcff3f55c7bc0d5c
ryanbrittes <ryanbrittes027@gmail.com>
2025-10-15 10:40:31 -0400

    [ADD] - Adicionada a função responsável por remover livros por id

--- app.py
+++ app.py
@@ -48,4 +48,12 @@ def add_books():
 
     return jsonify(books)
 
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_books_id(id):
+    for index, book in enumerate(books):
+        if book.get("id") == id:
+            del books[index]
+
+    return jsonify(books)
+    
 app.run(port=5000, host='localhost', debug=True)
